84e10d872f2c26443b2c12786ebe6eba44993fdc
Nishita-codes <nishitapatidar03@gmail.com>
2025-08-25 22:45:13 +0530

    added files

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const path =require("path");
+const fs = require("fs");
+const hbs = require("hbs");
 const app = express();
 require("./db/conn");
+
 const port = process.env.PORT || 3000;
 
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+
+app.use(express.static(static_path));
+app.set("view engine", "hbs");
+app.set("views",template_path);
+hbs.registerPartials(partials_path);
+
 app.get("/",(req,res) =>{
-res.send("hello from the thapatechnical")
-})
+
+res.render("index")
+});
 
 app.listen(port , ()=>{
     console.log(`server is running at port on ${port}`);
