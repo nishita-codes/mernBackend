3542f2eacdfbeca27ff23fa08d076ddec3c29dfa
Nishita-codes <nishitapatidar03@gmail.com>
2025-08-28 22:50:27 +0530

    added files

--- src/app.js
+++ src/app.js
@@ -12,14 +12,30 @@ const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 
+app.use(express.json()); //perfect for postman
+app.use(express.urlencoded({extended:false}));
+
 app.use(express.static(static_path));
 app.set("view engine", "hbs");
 app.set("views",template_path);
 hbs.registerPartials(partials_path);
+hbs.registerPartials(template_path);
 
 app.get("/",(req,res) =>{
-
 res.render("index")
+});
+
+app.get("/register",(req,res) =>{
+res.render("register")
+});
+
+// create a new user in our database
+app.post("/register", async (req,res) =>{
+   try{
+      
+   }catch(error){
+    res.status(400).send(error);
+   }
 });
 
 app.listen(port , ()=>{
